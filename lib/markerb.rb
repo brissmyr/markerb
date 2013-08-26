10282a0a261d376d91dc413c5027a5e46392be32
Johan Brissmyr <brissmyr@gmail.com>
2013-08-26 12:24:42 +0200

    Custom render options

--- lib/markerb.rb
+++ lib/markerb.rb
@@ -3,8 +3,9 @@ require "redcarpet"
 require "markerb/railtie"
 
 module Markerb
-  mattr_accessor :processing_options, :renderer
+  mattr_accessor :processing_options, :render_options, :renderer
   @@processing_options = {}
+  @@render_options = {}
   @@renderer = Redcarpet::Render::HTML
 
   class Handler
@@ -15,7 +16,7 @@ module Markerb
     def call(template)
       compiled_source = erb_handler.call(template)
       if template.formats.include?(:html)
-        "Redcarpet::Markdown.new(Markerb.renderer, Markerb.processing_options).render(begin;#{compiled_source};end).html_safe"
+        "Redcarpet::Markdown.new(Markerb.renderer.new(Markerb.render_options), Markerb.processing_options).render(begin;#{compiled_source};end).html_safe"
       else
         compiled_source
       end
